137b365670707528dad11a0e53c1f795e30ca4fc
Universe-Liu <842124824@qq.com>
2020-11-10 16:32:10 +0800

    05

--- 11/1105_SquareofasortedArray.cpp
+++ 11/1105_SquareofasortedArray.cpp
@@ -4,20 +4,15 @@ using namespace std;
 vector<int> SquaresortedAay(vector<int>& A){
     vector<int> ans(A.size(),0);
     if(A.size()){
-        if(A.size()==1){
-            ans.push_back(A[0]*A[0]);
-        }
-        else{
-            int i=0,j=A.size()-1,k=A.size()-1;
-            while(i<=j){
-                if(A[i]*A[i]>A[j]*A[j]){
-                    ans[k--]=A[i]*A[i];
-                    i++;
-                }
-                else{
-                    ans[k--]=A[j]*A[j];
-                    j--;
-                }
+        int i=0,j=A.size()-1,k=A.size()-1;
+        while(i<=j){
+            if(A[i]*A[i]>A[j]*A[j]){
+                ans[k--]=A[i]*A[i];
+                i++;
+            }
+            else{
+                ans[k--]=A[j]*A[j];
+                j--;
             }
         }
     }
